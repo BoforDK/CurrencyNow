c1e64079ef5144685dea0106d5fd90a2b9e8d3e5
Alexander Grigorov <BoforDK@gmail.com>
2025-01-18 21:52:06 +0100

    ✨ Implement currency list

--- Features/CurrencyList/Sources/CurrencyListFeature.swift
+++ Features/CurrencyList/Sources/CurrencyListFeature.swift
@@ -8,18 +8,73 @@
 import SwiftUI
 import ComposableArchitecture
 import AppCore
+import AppDependencies
 
 @Reducer
 public struct CurrencyListFeature {
+    @Dependency(\.exchangeRateClient) var exchangeRateClient
+    
     public init() {}
 
     @ObservableState
     public struct State: Equatable {
-        public init() {}
+        @Shared(.appStorage("sharedCurrencyCode")) var sharedCurrencyCode: String? = nil
+        
+        var screenState: ScreenState
+        
+        var currencies: [Currency]
+        var exchangeRates: [ExchangeRate]
+        
+        var isCurrenciesSheetPresented: Bool
+        var selectedCurrencyCode: CurrencyCode?
+        
+        public init(
+            screenState: ScreenState = .loading,
+            currencies: [Currency] = [],
+            exchangeRates: [ExchangeRate] = [],
+            isCurrenciesSheetPresented: Bool = false,
+            selectedCurrency: CurrencyCode? = nil
+        ) {
+            self.screenState = screenState
+            self.currencies = currencies
+            self.exchangeRates = exchangeRates
+            self.isCurrenciesSheetPresented = isCurrenciesSheetPresented
+            self.selectedCurrencyCode = selectedCurrencyCode
+        }
+        
+        var interactiveCurrencySheetDismissDisabled: Bool {
+            selectedCurrencyCode == nil
+        }
+        
+        var navigationTitle: String {
+            if let selectedCurrencyCode {
+                "Exchange Rate: \(selectedCurrencyCode.value)"
+            } else {
+                "Exchange Rate"
+            }
+        }
     }
 
-    public enum Action {
-        case goToDetail
+    public enum Action: ViewAction {
+        case onAppear
+        
+        case fetchCurrencies
+        case fetchCurrenciesResult(Result<[Currency], Error>)
+        
+        case fetchExchangeRates(CurrencyCode)
+        case fetchExchangeRatesResult(Result<[ExchangeRate], Error>)
+        
+        case selectCurrency(CurrencyCode)
+        
+        case view(View)
+
+        public enum Alert: Equatable, Sendable {}
+
+        @CasePathable
+        public enum View: BindableAction, Sendable {
+            case binding(BindingAction<State>)
+            case goToDetail
+        }
         
         case delegate(Delegate)
         public enum Delegate {
@@ -28,17 +83,102 @@ public struct CurrencyListFeature {
     }
 
     public var body: some Reducer<State, Action> {
+        BindingReducer(action: \.view)
+        
         Reduce(mainReducer)
     }
     
     private func mainReducer( _ state: inout State, action: Action) -> Effect<Action> {
         switch action {
-        case .goToDetail:
-            return .send(.delegate(.goToDetail))
+        case .onAppear:
+            return .send(.fetchCurrencies)
+            
+        case .fetchCurrencies:
+            return .run { send in
+                do {
+                    let allCurrencies = try await exchangeRateClient.allCurrencies()
+                    
+                    await send(.fetchCurrenciesResult(.success(allCurrencies)))
+                } catch {
+                    await send(.fetchCurrenciesResult(.failure(error)))
+                }
+            }
+            
+        case let .fetchCurrenciesResult(result):
+            switch result {
+            case let .success(data):
+                state.currencies = data
+                
+                if
+                    let code = state.sharedCurrencyCode,
+                    let currency = state.currencies.first(
+                        where: {
+                            $0.currencyCode.value == code
+                        }
+                    )
+                {
+                    return .send(.selectCurrency(currency.currencyCode))
+                } else {
+                    state.isCurrenciesSheetPresented = true
+                    
+                    return .none
+                }
+                
+            case .failure:
+                state.screenState = .error
+                
+                return .none
+            }
+            
+        case let .fetchExchangeRates(currencyCode):
+            return .run { send in
+                do {
+                    let exchangeRates = try await exchangeRateClient.exchangeRates(
+                        fromDate: .now.subtractDays(count: 1),
+                        toDate: .now,
+                        currencyCode: currencyCode
+                    )
+                    
+                    await send(.fetchExchangeRatesResult(.success(exchangeRates)))
+                } catch {
+                    await send(.fetchExchangeRatesResult(.failure(error)))
+                }
+            }
+            
+        case let .fetchExchangeRatesResult(result):
+            switch result {
+            case let .success(data):
+                state.screenState = .loaded
+                state.exchangeRates = data
+                
+                return .none
+            case .failure:
+                state.screenState = .error
+                
+                return .none
+            }
+            
+        case let .selectCurrency(code):
+            state.isCurrenciesSheetPresented = false
+            state.selectedCurrencyCode = code
+            state.$sharedCurrencyCode.withLock{
+                $0 = code.value
+            }
+            state.screenState = .loading
+            
+            return .send(.fetchExchangeRates(code))
+            
+        case let .view(viewAction):
+            switch viewAction {
+            case .binding:
+                return .none
+                
+            case .goToDetail:
+                return .send(.delegate(.goToDetail))
+            }
             
         case .delegate:
             return .none
         }
     }
 }
-
--- Features/CurrencyList/Sources/CurrencyListView.swift
+++ Features/CurrencyList/Sources/CurrencyListView.swift
@@ -6,23 +6,75 @@
 //
 
 import SwiftUI
-import AppCore
 import ComposableArchitecture
+import AppCore
+import AppUI
 
+@ViewAction(for: CurrencyListFeature)
 public struct CurrencyListView: View {
-    @Bindable var store: StoreOf<CurrencyListFeature>
+    @Bindable private var store: StoreOf<CurrencyListFeature>
     
-    public init(store: StoreOf<CurrencyListFeature>) {
+    @ScaledMetric(relativeTo: .body) var toolBarIconSize: CGFloat = 15
+    
+    public init(
+        store: StoreOf<CurrencyListFeature>
+    ) {
         self.store = store
     }
 
     public var body: some View {
-        VStack {
-            Text("Currency")
-            
-            Button("Go to detail") {
-                store.send(.goToDetail)
+        ZStack {
+            ScrollView {
+                VStack {
+                    ForEach(store.exchangeRates, id: \.currencyCode) { exchangeRate in
+                        ExchangeRateItem(
+                            exchangeRate: exchangeRate,
+                            comparedCurrency: store.selectedCurrencyCode
+                        )
+                    }
+                }
             }
+            .toolbar {
+                ToolbarItem {
+                    currencySheetButton
+                }
+            }
+            .screenState(store.screenState)
+        }
+        .task {
+            store.send(.onAppear)
+        }
+        .navigationTitle(store.navigationTitle)
+        .sheet(
+            isPresented: $store.isCurrenciesSheetPresented,
+            content: currencySheet
+        )
+    }
+    
+    private func currencySheet() -> some View {
+        ScrollView {
+            VStack {
+                ForEach(store.currencies, id: \.currencyCode) { currency in
+                    CurrencyItem(
+                        currency: currency,
+                        action: {
+                            store.send(.selectCurrency(currency.currencyCode))
+                        }
+                    )
+                }
+            }
+        }
+        .interactiveDismissDisabled(store.interactiveCurrencySheetDismissDisabled)
+    }
+
+    private var currencySheetButton: some View {
+        Button {
+            store.send(.view(.set(\.isCurrenciesSheetPresented, true)))
+        } label: {
+            Image(systemName: "arrow.left.arrow.right")
+                .resizable()
+                .scaledToFit()
+                .frame(width: toolBarIconSize, height: toolBarIconSize)
         }
     }
 }
@@ -31,7 +83,11 @@ public struct CurrencyListView: View {
 #Preview {
     CurrencyListView(
         store: .init(
-            initialState: .init(),
+            initialState: .init(
+                screenState: .loaded,
+                currencies: .mock,
+                exchangeRates: .mock
+            ),
             reducer: EmptyReducer.init
         )
     )
